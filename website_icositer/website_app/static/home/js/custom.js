5a12cd0cfc61d27c54173023470b716ad86e6295
Toolop <toolop2312@gmail.com>
2021-07-19 16:14:33 +0700

    menambahkan competition

--- website_icositer/website_app/static/home/js/custom.js
+++ website_icositer/website_app/static/home/js/custom.js
@@ -8,11 +8,7 @@ var play = true;
 var reset = false;
 
 var berita1 = "A press on a button translates all images in the respective direction along the x-axis. An image on the edge, is set transparent outerImg.style.opacity = '0'; and translated to the other side. You can add or remove image elements in HTML and it still works.";
-<<<<<<< HEAD
 var berita2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.";
-=======
-var berita2 = "This is only for getting your data models into JS. For manipulating them, you should create some views that will get called from JS (by AJAX). They should probably return JSON. Check out http://djangosnippets.org/snippets/622/ or http://djangosnippets.org/snippets/2102/ on how to simply return only JSON from a view.";
->>>>>>> 9ea83b64a1cba3cbec7e280f8f7500266953389a
 var berita3 = "berita3";
 
 $('#beritateks').html(berita1);
